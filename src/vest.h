06c506f85e77d040bf7addbdc7cb8c6a1d07797b
Benjamin Morgan <bmorgan5@uw.edu>
2016-08-02 23:42:42 -0700

    vest.h is a top level header file that all files will probably need to include

---
+++ src/vest.h
@@ -0,0 +1,15 @@
+#ifndef VEST_H
+#define VEST_H
+
+#include "WProgram.h"
+#include <FastLED.h>
+
+#define NUM_LEDS 300
+
+#define LED_PIN 12
+#define CHIPSET WS2812B
+#define COLOR_ORDER GRB
+#define BRIGHTNESS  200
+#define FRAMES_PER_SECOND 60
+
+#endif /*VEST_H*/
