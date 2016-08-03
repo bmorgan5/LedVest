51e9a01f2350fc5924effe73e449275a9bb29590
Benjamin Morgan <bmorgan5@uw.edu>
2016-08-02 23:43:17 -0700

    fire2012 demo on 300 LEDS!!

---
+++ src/fire2012.h
@@ -0,0 +1,7 @@
+#ifndef FIRE2012_H
+#define FIRE2012_H
+
+void setup_fire();
+void loop_fire();
+
+#endif /*FIRE2012_H*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,78 @@
-#include "WProgram.h"
+
+#include "vest.h"
+#include "fire2012.h"
+
+CRGB leds[NUM_LEDS];
 
 extern "C" 
 
+// void cycle_two_led(){
+// 	while(1) {
+// 				// Currently only testing with 2 LEDs
+// 		leds[0] = CRGB(255,0,0);
+// 		leds[1] = CRGB(0,255,0);
+// 		FastLED.show();
+// 		delay(500);
+
+// 		leds[0] = CRGB(0,255,0);
+// 		leds[1] = CRGB(0,0, 255);
+// 		FastLED.show();
+// 		delay(500);
+
+// 		leds[0] = CRGB(0,0,255);
+// 		leds[1] = CRGB(255,0,0);
+// 		FastLED.show();
+// 		delay(500);
+// 	}
+// }
+
 int main(void)
 {
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
+
+	setup_fire();
+
+	while(1){
+		loop_fire();
 	}
+
+	
+	// delay(2000);
+
+	// FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	// // FastLED.setBrightness(CRGB(255,255,255));
+
+	// pinMode(13, OUTPUT);
+	// digitalWriteFast(13, HIGH);
+
+	// leds[0] = CRGB(255,0,0);
+	// leds[1] = CRGB(0,255,0);
+	// leds[2] = CRGB(0,0,255);
+	// leds[3] = CRGB::White;
+	// FastLED.show();
+
+	// while(1) {
+	// 	delay(100000);
+	// }
+
+	// cycle_two_led();
+
+	// while (1) {
+		// leds[0] = CRGB(255,255,255); 
+		// leds[1] = CRGB(255,255,255);
+		// leds[2] = CRGB(0,255,0);
+		// leds[3] = CRGB(0,0,255);
+		// leds[4] = CRGB(0,0,255);
+		// leds[5] = CRGB(0,0,255);
+		// FastLED.show();
+		// delay(1000);
+
+
+
+
+
+		// digitalWriteFast(13, HIGH);
+		// delay(500);
+		// digitalWriteFast(13, LOW);
+		// delay(500);
+	// }
 }
