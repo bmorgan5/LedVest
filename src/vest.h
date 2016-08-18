47a13fd25981f4517cc07dbc632ee8d8f97d507c
Benjamin Morgan <bmorgan5@uw.edu>
2016-08-17 23:35:25 -0700

    Updated global settings

--- src/vest.h
+++ src/vest.h
@@ -3,13 +3,18 @@
 
 #include "WProgram.h"
 #include <FastLED.h>
-
-#define NUM_LEDS 300
+#include "matrix.h"
 
 #define LED_PIN 12
 #define CHIPSET WS2812B
 #define COLOR_ORDER GRB
-#define BRIGHTNESS  200
+
+#define BRIGHTNESS  64
 #define FRAMES_PER_SECOND 60
+#define FPS FRAMES_PER_SECOND
+
+#define NUM_LEDS 600
+#define WIDTH 24
+#define HEIGHT 25
 
 #endif /*VEST_H*/
