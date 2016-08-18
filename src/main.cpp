8a49d2163ed00fcf1af75736fdeb8a52a49e7920
Benjamin Morgan <bmorgan5@uw.edu>
2016-08-17 23:42:05 -0700

    Updated and cleaner main

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,46 @@
-
 #include "vest.h"
 #include "fire2012.h"
+#include "colors.h"
+#include "gifs.h"
 
 CRGB leds[NUM_LEDS];
 
 extern "C" 
 
-// void cycle_two_led(){
-// 	while(1) {
-// 				// Currently only testing with 2 LEDs
-// 		leds[0] = CRGB(255,0,0);
-// 		leds[1] = CRGB(0,255,0);
-// 		FastLED.show();
-// 		delay(500);
+void off(uint8_t secs) {
+	for(uint16_t i = 0; i < NUM_LEDS; i++ ){
+		leds[i] = CRGB::Black;
+	}
 
-// 		leds[0] = CRGB(0,255,0);
-// 		leds[1] = CRGB(0,0, 255);
-// 		FastLED.show();
-// 		delay(500);
+	FastLED.show();
+	FastLED.delay(1000 * secs);
+}
 
-// 		leds[0] = CRGB(0,0,255);
-// 		leds[1] = CRGB(255,0,0);
-// 		FastLED.show();
-// 		delay(500);
-// 	}
-// }
+void setup() {
+	delay(2000); // sanity delay
+	FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+	FastLED.setCorrection( TypicalLEDStrip );
+	FastLED.setBrightness( BRIGHTNESS );
+}
 
 int main(void)
 {
+	pinMode(13, OUTPUT);
+	digitalWriteFast(13, HIGH);
 
-	setup_fire();
+	setup();
+
 
 	while(1){
-		loop_fire();
+		// nyan_cat();
+		off(1);
+		// loop_fire();
+		// test_matrix();
+		// test_brightness();
+		// test_correction();
+		// test_temperature();
+		// test_sparkles();
 	}
-
-	
-	// delay(2000);
-
-	// FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-	// // FastLED.setBrightness(CRGB(255,255,255));
-
-	// pinMode(13, OUTPUT);
-	// digitalWriteFast(13, HIGH);
-
-	// leds[0] = CRGB(255,0,0);
-	// leds[1] = CRGB(0,255,0);
-	// leds[2] = CRGB(0,0,255);
-	// leds[3] = CRGB::White;
-	// FastLED.show();
-
-	// while(1) {
-	// 	delay(100000);
-	// }
-
-	// cycle_two_led();
-
-	// while (1) {
-		// leds[0] = CRGB(255,255,255); 
-		// leds[1] = CRGB(255,255,255);
-		// leds[2] = CRGB(0,255,0);
-		// leds[3] = CRGB(0,0,255);
-		// leds[4] = CRGB(0,0,255);
-		// leds[5] = CRGB(0,0,255);
-		// FastLED.show();
-		// delay(1000);
-
-
-
-
-
-		// digitalWriteFast(13, HIGH);
-		// delay(500);
-		// digitalWriteFast(13, LOW);
-		// delay(500);
-	// }
 }
+	
+	
